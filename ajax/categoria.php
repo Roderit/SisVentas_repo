a3e633c1e70a94c61362216e073f4dd546a4c63b
Roderit <roderit.orocu@utp.ac.pa>
2024-01-10 15:46:26 -0500

    Creación de archivo ajax para funciones de categoría

---
+++ ajax/categoria.php
@@ -0,0 +1,45 @@
+<?php
+
+    require_once "../modelos/categoria.php";
+
+    $categoria = new Categoria();
+
+    $idcategoria = isset($_POST["idcategoria"])? limpiarCadena($_POST["idcategoria"]):"";
+
+    $nombre = isset($_POST["nombre"])? limpiarCadena($_POST["nombre"]):"";
+
+    $descripcion = isset($_POST["descripcion"])? limpiarCadena($_POST["descripcion"]):"";
+
+
+    switch($_GET["op"]){
+        case 'guardareditar':
+            if(empty($idcategoria)){
+                $respuesta = $categoria->insertar($nombre, $descripcion);
+                echo $respuesta? "Categoría registrada" : "Categoría no se pudo registrar";
+            }else{
+                $respuesta = $categoria->editar($idcategoria, $nombre, $descripcion);
+                echo $respuesta? "Categoría actualizada" : "Categoría no se pudo actualizar";
+            }
+        break;
+
+        case 'desactivar':
+            $respuesta = $categoria->desactivar($idcategoria);
+            echo $respuesta? "Categoría desactivada" : "Categoría no se pudo desactivar";
+        break;
+
+        case 'activar':
+            $respuesta = $categoria->activar($idcategoria);
+            echo $respuesta? "Categoría activada" : "Categoría no se pudo activar";
+        break;
+
+        case 'mostrar':
+            $respuesta = $categoria->mostrar($idcategoria);
+            echo json_encode($respuesta);
+        break;
+
+        case 'listar':
+            
+        break;
+
+    }
+?>
